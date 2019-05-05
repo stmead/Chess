ead4b0b05c17c1d65621660de91d291e4b3fe994
stmead <46060582+stmead@users.noreply.github.com>
2019-05-04 21:05:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <>
+#include <iostream>
 #include <cstdlib>
 #include "ChessBoard.h"
 #include "ChessPieces.h"
 #include "Check.h"
 #include "gui.h"
+
+class ChessGame {
+private:
+  ChessBoard board;
+  char PlayerTurn;
+public:
+  ChessGame() {
+    PlayerTurn = ‘W’;
+  }
+  void Start() {
+    
+  }
+};
 
 int main() {
   ChessGame game;
